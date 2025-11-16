edcf30464a18215c0cdd9ce49662752b9140fc03
Lucas Fry <lucas.e.fry@outlook.com>
2025-11-16 16:50:34 -0600

    Added root file, css and js

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+function topNavigation() {
+    let x = document.getElementById('topnav');
+    if (x.className = 'topnav') {
+        x.className += ' reponsive';
+    } else {
+        x.className = 'topnav';
+    }
+}
